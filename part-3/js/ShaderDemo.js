36795a7f564d8b22c85776a353bc8e75b727b743
Ray McClure <raymcclure@Rays-MacBook-Pro.local>
2018-05-26 02:25:33 -0700

    part-3

--- part-3/js/ShaderDemo.js
+++ part-3/js/ShaderDemo.js
@@ -11,19 +11,49 @@ ShaderDemo = {
 
     "uniform float uTime;",
 
-    "uniform sampler2D uTex;",
+    "// Simplex 2D noise",
+    
+    // "vec3 permute(vec3 x) { return mod(((x*34.0)+1.0)*x, 289.0); }",
+
+    // "float snoise(vec2 v){",
+    //   "const vec4 C = vec4(0.211324865405187, 0.366025403784439, -0.577350269189626, 0.024390243902439);",
+    //   "vec2 i  = floor(v + dot(v, C.yy) );",
+    //   "vec2 x0 = v -   i + dot(i, C.xx);",
+    //   "vec2 i1;",
+    //   "i1 = (x0.x > x0.y) ? vec2(1.0, 0.0) : vec2(0.0, 1.0);",
+    //   "vec4 x12 = x0.xyxy + C.xxzz;",
+    //   "x12.xy -= i1;",
+    //   "i = mod(i, 289.0);",
+    //   "vec3 p = permute( permute( i.y + vec3(0.0, i1.y, 1.0 ))",
+    //   "+ i.x + vec3(0.0, i1.x, 1.0 ));",
+    //   "vec3 m = max(0.5 - vec3(dot(x0,x0), dot(x12.xy,x12.xy),",
+    //   "  dot(x12.zw,x12.zw)), 0.0);",
+    //   "m = m*m ;",
+    //   "m = m*m ;",
+    //   "vec3 x = 2.0 * fract(p * C.www) - 1.0;",
+    //   "vec3 h = abs(x) - 0.5;",
+    //   "vec3 ox = floor(x + 0.5);",
+    //   "vec3 a0 = x - ox;",
+    //   "m *= 1.79284291400159 - 0.85373472095314 * ( a0*a0 + h*h );",
+    //   "vec3 g;",
+    //   "g.x  = a0.x  * x0.x  + h.x  * x0.y;",
+    //   "g.yz = a0.yz * x12.xz + h.yz * x12.yw;",
+    //   "return 130.0 * dot(m, g);",
+    // "}",
 
     "void main( void ) {",
 
       "vUv = uv;",
 
-      "float offsetY = sin( ( uTime * 0.001 ) + vUv.x );",
-
-      "vec4 texture = texture2D( uTex, vec2( vUv.x, mod( offsetY + vUv.y, 1.0 ) ) );",
-
       "vec3 p = position;",
 
-      // "p.y += offsetY;",
+      "float offsetZ = 0.0;",
+      
+      // "offsetZ += snoise(vec2(vUv.x * 10.0, (vUv.y * 10.0) + (uTime * 0.001)));",
+
+      // "offsetZ *= 0.5;",
+
+      // "p.z += offsetZ;",
 
       "gl_Position = projectionMatrix * modelViewMatrix * vec4( p, 1.0 );",
 
@@ -53,9 +83,9 @@ ShaderDemo = {
       // "offsetX = cos( ( uTime * 0.001 ) + vUv.y ) + offsetX;",
       // "offsetY = sin( ( uTime * 0.001 ) + vUv.x ) + offsetY;",
 
-      "vec4 texture = texture2D( uTex, vec2( mod( offsetX, 1.0 ), mod( offsetY, 1.0 ) ) );",
+      "vec4 t = texture2D( uTex, vec2( mod( offsetX, 1.0 ), mod( offsetY, 1.0 ) ) );",
 
-      "gl_FragColor = texture;",
+      "gl_FragColor = t;",
 
     "}"
 
